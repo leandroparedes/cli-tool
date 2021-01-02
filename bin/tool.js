fb0ca299499f997b0df556709beedd11f7c1d5c4
Leandro Paredes <leandroparedes6@gmail.com>
2021-01-02 14:12:50 -0300

    create module from folder template

--- bin/tool.js
+++ bin/tool.js
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 
-console.log("CLI tool");
+const { program } = require("commander");
+
+program
+    .version(`CLI Tool v${require("../package").version}`)
+    .description("CLI Tool program for development workflow");
+
+program
+    .command("create <module-name>")
+    .description("Create a new module")
+    .option("-s, --store", "Add vuex module")
+    .action((moduleName, cmd) => {
+        const options = cleanArgs(cmd);
+
+        require("../lib/create")(moduleName, options);
+    });
+
+program.parse(process.argv);
+
+function camelize(str) {
+    return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ""));
+}
+
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs(cmd) {
+    const args = {};
+    cmd.options.forEach((o) => {
+        const key = camelize(o.long.replace(/^--/, ""));
+        // if an option is not present and Command has a method with the same name
+        // it should not be copied
+        if (typeof cmd[key] !== "function" && typeof cmd[key] !== "undefined") {
+            args[key] = cmd[key];
+        }
+    });
+    return args;
+}
