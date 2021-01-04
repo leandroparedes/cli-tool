346e76bf559327f299e5d3ca99b88bbb2d7d88ca
Leandro Paredes <leandroparedes6@gmail.com>
2021-01-03 23:11:34 -0300

    add prettier config in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ["google"],
+    extends: ["google", "prettier"],
     parserOptions: {
         ecmaVersion: 12,
         sourceType: "module",
